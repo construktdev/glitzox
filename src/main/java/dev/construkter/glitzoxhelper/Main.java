40faa34d932db2745258961af82b67224955371e
construktdev <construktai@gmail.com>
2025-10-02 18:50:24 +0200

    Add Version output in ready method

--- src/main/java/dev/construkter/glitzoxhelper/Main.java
+++ src/main/java/dev/construkter/glitzoxhelper/Main.java
@@ -20,6 +20,7 @@ public class Main extends ListenerAdapter {
 
     public static TextChannel logChannel;
     public static TextChannel announcementChannel;
+    public static final String VERSION = "1.0.1";
 
     public static void main(String[] args) {
         JDA jda = JDABuilder.createDefault(Token.get())
@@ -49,6 +50,7 @@ public class Main extends ListenerAdapter {
         EmbedBuilder embedBuilder = new EmbedBuilder()
                 .setTitle("GlitzoX Ready")
                 .setDescription("GlitzoX has initialized!")
+                .addField("Version", VERSION, true)
                 .setColor(Color.GREEN)
                 .setFooter("GlitzoX")
                 .setTimestamp(Instant.now());
